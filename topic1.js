b695da4ffad78de250da1d4d3bc39aa5bd6a8404
Volodymyr I Pankiv <vpankiv@softserveinc.com>
2026-01-28 18:51:16 +0200

    topic1

---
+++ topic1.js
@@ -0,0 +1,161 @@
+// topic1.js
+
+// alert("hello"); // First alert
+// alert("hello");
+// alert("hello");
+
+
+// console.log("12345");
+
+// змінні
+
+// let const var
+
+// let a = "hello";
+
+// a = "hi";
+
+// const a = "hello";
+
+// a = "hi";
+
+// console.log(a);
+
+// let name = "Volodymyr";
+
+// const PI 
+
+// let nameOfUser = "Volodymyr";
+
+// string
+// let a = "hello 3435 gdfgfe";
+// number, BigInt
+// let b = 12345;
+// let c = 1234567890123456789012345678901234567890n;
+// boolean
+// let d = true; // false
+// null
+// let e = 10
+// e = null
+// undefined
+// let f;
+// console.log(f);
+// Symbol
+// Object
+// let user = {
+//     name: "Volodymyr",
+//     age: 28,
+//     isAdmin: true
+// };
+
+// let x = 10;
+
+// x = 10;
+
+// console.log(typeof x);
+
+// alert("hello from topic1.js");
+
+// let name = prompt("Enter your name:");
+// let age = prompt("Enter your age:");
+
+// alert("Hello, " + name + " you are " + age + " years old");
+
+// let here = confirm("are you here?");
+
+// console.log(here);
+
+// string
+
+// % - залишок від ділення
+
+// console.log(5 % 2); // 1
+// console.log(4 % 2); // 0
+// console.log(10 % 3); // 1
+// console.log("hello" + " world");
+
+// console.log(true + "2")
+
+
+// console.log(10 + "5"); 
+// console.log(10 - "5");
+// console.log("10" / "2");
+// console.log("10" * "2");
+
+// console.log(2 ** 2); // 4
+// console.log(2 ** 3);
+// console.log(2 ** 4);
+
+// Math.pow(2, 3); // 2 в степені 3
+// Math.sqrt(16); // 4
+
+// let a = 10;
+
+// a += 5; // a = a + 5
+// a -= 3; // a = a - 3
+// a *= 2; // a = a * 2
+// a /= 4; // a = a / 4
+// a %= 3; // a = a % 3
+
+// a++; // a = a + 1
+// a--; // a = a - 1
+
+// console.log(10 / "a");
+
+// console.log(-10 / 0)
+
+// console.log(10 > 5);
+// console.log(10 < 5);
+// console.log(10 >= 10);
+// console.log(10 <= 5);
+// console.log(10 == 10);
+// console.log(10 != 5);
+
+
+// console.log(10 == 10); ===
+// 0 - false
+// 1 - true
+// "" - false
+// "hello" - true
+
+// console.log(1 == true);
+
+// console.log(1 === true);
+
+//  && - i
+//  || - abo
+//  ! - negation
+
+// console.log(5 > 3 && 10 > 7); // true
+// console.log(5 > 3 || 10 < 7); // true
+// console.log(!(5 > 3)); // false
+
+// тернарний оператор
+
+// (умова) ? вираз1 : вираз2
+
+// let age = 25;
+// // let access = (age >= 18) ? "allowed" : "denied";
+// // let access = (age >= 18 && age <= 60) ? "allowed" : "denied";
+// let access = (age <= 18 || age >= 60) ? "allowed" : "denied";
+
+// console.log(access);
+
+// if else
+
+// let age = 15;
+
+// if (age >= 18) {
+//     console.log("allowed");
+// } else{
+//     console.log("denied");
+// }
+// if (age >= 20 && age <=30){
+//     console.log("20-30");
+// } else if (age >30 && age <=40){
+//     console.log("30-40");
+// } else if(age >40 && age <=50){
+//     console.log("40-50");
+// } else {
+//     console.log("other age");
+// }
